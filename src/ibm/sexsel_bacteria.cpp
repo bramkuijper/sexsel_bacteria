3dfeb7ee3050d9f284cfd7f60be4e8bdf84fbfd7
bram <a.kuijper@ucl.ac.uk>
2020-06-15 15:11:10 +0100

    adding a birth event function

--- src/ibm/sexsel_bacteria.cpp
+++ src/ibm/sexsel_bacteria.cpp
@@ -20,13 +20,27 @@ std::uniform_real_distribution<> uniform(0.0,1.0);
 // file base name
 std::string base_name;
 
+// initial value of resistance
+double init_x = 0.0;
+
 // population size
 int const N = 10000;
 
 int const nplasmid_max = 2;
 
+// initial frequency of good plasmid 
+// in infected individuals
+double p_good_init = 0.0;
+double n_plasmid_init = 1;
+
+// density dependence
+double kappa = 1;
+
+// growth rate in absence of density dependence
+double b = 1;
+
 // number of timesteps that the simulation should run
-int number_timesteps = 10;
+int max_time = 10;
 
 // individual
 struct Individual
@@ -39,6 +53,8 @@ struct Individual
 
     // number of plasmids carried by individual
     int nplasmids;
+
+    double fraction_good;
 };
 
 typedef Individual Population[N];
@@ -55,6 +71,10 @@ int Ns = N - Ni;
 // initialize population
 void init_pop()
 {
+    // auxiliary variable to keep track
+    // of the fraction of good plasmids in each infected
+    double fraction_good;
+
     // initialize susceptibles
     for (int S_idx = 0; S_idx < Ns; ++S_idx)
     {
@@ -68,25 +88,102 @@ void init_pop()
         Infected[I_idx].x = init_x;
         Infected[I_idx].nplasmids = 1;
 
-        for (int plasmid_i = 0; n_plasmid_init; ++n_plasmid_init)
-        {
-            // randomly determine whether individual carries good / bad plasmid
-            Infected[I_idx].plasmid_good[plasmid_idx] = uniform(rng_r) < 0.5;
-        }
+        // initialize fraction of good plasmids
+        fraction_good = 0.0;
 
-//        Infected[I_idx].plasmid_good[
+        for (int plasmid_idx = 0; plasmid_idx < n_plasmid_init; ++plasmid_idx)
+        {
+            // determine whether individual carries good / bad plasmid
+            Infected[I_idx].plasmid_good[plasmid_idx] = uniform(rng_r) < p_good_init;
+        }
     }
 }//end void 
 
+// initialize the parameters through the command line
 void init_arguments(int argc, char ** argv)
 {
+    max_time = atof(argv[1]);
+    p_good_init = atof(argv[2]);
+    kappa = atof(argv[3]);
+    b = atof(argv[4]);
+    base_name = argv[5];
 
 }//end init_arguments()
+
+void write_parameters(std::ofstream &data_file)
+{
+    data_file << std::endl << std::endl
+        << "p_good_init" << ";" << p_good_init << std::endl
+        << "N" << ";" << N << std::endl
+        << "max_time" << ";" << max_time << std::endl
+        << "b" << ";" << b << std::endl
+        << "kappa" << ";" << kappa << std::endl
+        << "n_plasmid_init" << ";" << p_good_init << std::endl;
+
+} // end write_parameters()
+
+// birth event of an individual
+void birth(Individual &parent)
+{
+}
+
+// write headers to the datafile
+void write_data_headers(std::ofstream &data_file)
+{
+    std::vector<double> 
+} // end write_data_headers()
+
+// setup a distribution of events and choose
+// which event to do
+void event_chooser(int const time_step)
+{
+    // cumulative birth probability
+    double cumul_birth = 0.0;
+
+    // initialize a vector that contains all the weights 
+    // re an individual's birth
+    std::vector <double> birth_weights;
+
+    // birth of a susceptible host (from another one) 
+    // is easy as we do not need to
+    // track any plasmids.
+    double prob_birth_susceptible = cumul_birth = b * (1.0 - kappa * N) * S;
+    birth_weights.push_back(prob_birth_susceptible);
+
+
+
+    // now go through the infected hosts and determine
+    // their plasmid phenotype 
+    for (int inf_idx = 0; inf_idx < Ni; ++inf_idx)
+    {
+        fraction_good = 0.0;
+        for (int plasmid_idx = 0; plasmid_idx < Infected.nplasmids; ++plasmid_idx)
+        {
+        }
+
+        birth_weights.push_back(
+    }
+}
 
 int main(int argc, char **argv)
 {
     init_arguments(argc, argv);
+    
+    // initialize file to write data to
+    std::string file_name = base_name + ".csv";
+    std::ofstream data_file{file_name};
+    write_data_headers(data_file);
+
+    // initialize the population
     init_pop();
+
+    // numerically iterate the simulation
+    for (int time_idx = 0; time_idx < max_time; ++time_idx)
+    {
+        event_chooser(time_idx);
+    }
+    
+    write_parameters(data_file);
 }
 
 

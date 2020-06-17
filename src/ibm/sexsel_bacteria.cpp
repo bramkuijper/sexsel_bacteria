c69ce472a4a7ed7e683b303ef57c7837131a2e7d
bram <a.kuijper@ucl.ac.uk>
2020-06-17 15:02:33 +0100

    working on birth function

--- src/ibm/sexsel_bacteria.cpp
+++ src/ibm/sexsel_bacteria.cpp
@@ -37,7 +37,29 @@ double n_plasmid_init = 1;
 double kappa = 1;
 
 // growth rate in absence of density dependence
-double b = 1;
+double bmax = 1;
+
+// resistance cost factor 
+double c = 1;
+
+// loss rate of good plasmid
+double gamma_G = 0.0;
+// loss rate of bad plasmid
+double gamma_B = 0.0;
+
+// infection rate of good plasmid
+double psi_G = 0.0;
+// infection rate of bad plasmid
+double psi_B = 0.0;
+
+// death rate susceptible
+double d = 0.0;
+
+// death rate host infected only with good plasmids
+double dG = 0.0;
+
+// death rate host infected only with bad plasmids
+double dB = 0.0;
 
 // number of timesteps that the simulation should run
 int max_time = 10;
@@ -55,6 +77,7 @@ struct Individual
 
     // number of plasmids carried by individual
     int nplasmids;
+    int nplasmids_good;
 
     // actual plasmid phenotype is then the fraction
     // of good plasmids (i.e., additive effects)
@@ -90,10 +113,10 @@ void init_pop()
     for (int I_idx = 0; I_idx < Ni; ++I_idx)
     {
         Infected[I_idx].x = init_x;
-        Infected[I_idx].nplasmids = 1;
+        Infected[I_idx].nplasmids = n_plasmid_init;
 
         // initialize fraction of good plasmids
-        fraction_good = 0.0;
+        n_good = 0;
 
         for (int plasmid_idx = 0; plasmid_idx < n_plasmid_init; ++plasmid_idx)
         {
@@ -108,8 +131,11 @@ void init_pop()
                 Infected[I_idx].plasmid_good[plasmid_idx] = false;
             }
 
-            Infected[I_idx].fraction_good = (double)n_good/n_plasmid_init;
         }
+
+        Infected[I_idx].nplasmids_good = n_good;
+        assert(Infected[I_idx].nplasmids_good <= Infected[I_idx].nplasmids);
+        Infected[I_idx].fraction_good = (double)n_good/n_plasmid_init;
     }
 }//end void 
 
@@ -119,7 +145,15 @@ void init_arguments(int argc, char ** argv)
     max_time = atof(argv[1]);
     p_good_init = atof(argv[2]);
     kappa = atof(argv[3]);
-    b = atof(argv[4]);
+    bmax = atof(argv[4]);
+    c = atof(argv[4]);
+    gamma_G = atof(argv[5]);
+    gamma_B = atof(argv[6]);
+    psi_G = atof(argv[7]);
+    psi_B = atof(argv[8]);
+    d = atof(argv[9]);
+    dG = atof(argv[10]);
+    dB = atof(argv[11]);
     base_name = argv[5];
 
 }//end init_arguments()
@@ -130,51 +164,230 @@ void write_parameters(std::ofstream &data_file)
         << "p_good_init" << ";" << p_good_init << std::endl
         << "N" << ";" << N << std::endl
         << "max_time" << ";" << max_time << std::endl
-        << "b" << ";" << b << std::endl
+        << "bmax" << ";" << bmax << std::endl
+        << "c" << ";" << c << std::endl
+        << "gamma_G" << ";" << gamma_G << std::endl
+        << "gamma_B" << ";" << gamma_B << std::endl
+        << "psi_G" << ";" << psi_G << std::endl
+        << "psi_B" << ";" << psi_B << std::endl
+        << "d" << ";" << d << std::endl
+        << "dG" << ";" << dG << std::endl
+        << "dB" << ";" << dB << std::endl
         << "kappa" << ";" << kappa << std::endl
         << "n_plasmid_init" << ";" << p_good_init << std::endl;
 
 } // end write_parameters()
 
-// birth event of an individual
-void birth(Individual &parent)
+double mutation()
 {
+
+}
+    
+
+
+// birth event of an individual
+void birth(Individual &parent, bool parent_susceptible)
+{
+    Individual kid;
+
+    kid.x = mutation(parent.x);
+
+    clamp(kid.x, 0.0, 1.0);
+
 }
 
 // write headers to the datafile
 void write_data_headers(std::ofstream &data_file)
 {
-    std::vector<double> 
+    data_file << "time;mean_resistance;Ns;Ni;mean_nplasmid;" << std::endl;
 } // end write_data_headers()
+
+// fecundity function accounting for plasmid behaviour
+// (good vs bad)
+double F(double const fraction_good)
+{
+    // this should be something else, but now it is just
+    // fraction good, i.e, linear decline
+    return(fraction_good);
+}
+
+// fecundity function that accounts for costly resistance,
+// as in Gandon & Vale eq (2)
+double b(double const resistance)
+{
+    return(bmax * exp(-c * resistance));
+}
+
+// calculate the odds of a plasmid loss event
+// and return this is in gamma val
+// also determine whether this is a loss of a 
+// good or a bad plasmid and return this in 
+// loss_good
+void gamma_loss(double &gamma_good, double &gamma_bad, Individual &host)
+{
+    assert(host.nplasmids_good <= host.nplasmids);
+
+    gamma_good = host.nplasmids_good * gamma_G;
+    gamma_bad = (host.nplasmids - host.nplasmids_good) * gamma_B;
+}
 
 // setup a distribution of events and choose
 // which event to do
 void event_chooser(int const time_step)
 {
-    // cumulative birth probability
-    double cumul_birth = 0.0;
+    // some auxiliary variables to store temporary values about rates
+    double rate_birth, rate_loss_good, rate_loss_bad, rate_infect;
 
-    // initialize a vector that contains all the weights 
+    // make a vector of total rates 
+    // (these are the sums of all the individual 
+    // infection, death and loss rates)
+    //
+    // we use this later to establish which of the events
+    // will be picked
+    int n_events = 10;
+    std::vector <double> total_rates(n_events, 0.0);
+
+    // declare a vector that contains all the rates
     // re an individual's birth
-    std::vector <double> birth_weights;
+    std::vector <double> birth_rates_susceptible;
+    std::vector <double> birth_rates_infected;
 
-    // go through all susceptibles and calculate birth probs
+    // declare vectors containing infection rates of susceptibles
+    // with good and bad plasmids respectively
+    std::vector <double> infection_rate_susceptible_good;
+    std::vector <double> infection_rate_susceptible_bad;
+    
+    // same for infected (aka rates as co-infection)
+    std::vector <double> infection_rate_infected_good;
+    std::vector <double> infection_rate_infected_bad;
+
+    // declare vectors containing rates of
+    // plasmid loss (good and bad respectively)
+    std::vector <double> loss_rates_good;
+    std::vector <double> loss_rates_bad;
+
+    std::vector <double> death_rate_infected;
+    
+    double cumul_loss_rate_good = 0.0;
+    double cumul_loss_rate_bad = 0.0;
+
+    // go through all susceptibles and calculate birth
+    // and infection rates
     for (int S_idx = 0; S_idx < Ns; ++S_idx)
     {
-        birth_weights.push_back(prob_birth_susceptible);
+        // 1. Birth of susceptibles
+        rate_birth = b(Susceptible[S_idx].x) * (1.0 - kappa * N);
+        birth_rates_susceptible.push_back(rate_birth);
+
+        total_rates[0] += rate_birth;
+
+        // 2. infection of susceptible by good plasmid
+        // note that infection rates are independent of the number of 
+        // bacteria already infected
+        rate_infect = (1.0 - Susceptible[S_idx].x) * psi_G;
+        infection_rate_susceptible_good.push_back(rate_infect);
+        
+        total_rates[1] += rate_infect;
+        
+        // 3. infection of susceptible by bad plasmid
+        // note that infection rates are independent of the number of 
+        // bacteria already infected
+        rate_infect = (1.0 - Susceptible[S_idx].x) * psi_B;
+        infection_rate_susceptible_bad.push_back(rate_infect);
+        
+        total_rates[2] += rate_infect;
     }
 
-    // birth of a susceptible host (from another one) 
-    // is easy as we do not need to
-    // track any plasmids.
+    // 4. Deaths susceptibles
+    double death_rate_S = Ns * d;
 
-    // now go through the infected hosts and determine
-    // their plasmid phenotype 
+    total_rates[3] += death_rate_S;
+
+    // now go through the infected hosts 
     for (int inf_idx = 0; inf_idx < Ni; ++inf_idx)
     {
-        fecundity(Infected[inf_idx].fraction_good) * b
+        // 5. birth infected host
+        rate_birth = F(Infected[inf_idx].fraction_good) * 
+            b(Infected[inf_idx].x) * (1.0 - kappa * N);
+        
+        birth_rates.push_back(rate_birth);
+
+        total_rates[4] += rate_birth;
+
+        // 6, 7. loss of a good or bad plasmid 
+        // a single plasmid loss event
+        gamma_loss(rate_loss_good, rate_loss_bad, Infected[inf_idx]);
+
+        cumul_loss_rate_bad += rate_loss_bad;
+        cumul_loss_rate_good += rate_loss_good;
+
+        loss_rates_good.push_back(rate_loss_good);
+        total_rates[5] += rate_loss_good;
+
+        loss_rates_bad.push_back(rate_loss_bad);
+        total_rates[6] += rate_loss_bad;
+
+        // 8. death rate infected
+        death_rate = Infected[inf_idx].fraction_good * dG + 
+            (1.0 - Infected[inf_idx].fraction_good) * dB;
+
+        death_rate_infected.push_back(death_rate);
+        total_rates[7] += death_rate;
+
+        // 9. infection with another plasmid
+        co_infection_good = sigma * (1.0 - Infected[inf_idx].x) * psi_G;
+        infection_rate_infected_good.push_back(co_infection_good);
+        
+        total_rates[8] += co_infection_good;
+
+        co_infection_bad = sigma * (1.0 - Infected[inf_idx].x) * psi_B;
+        infection_rate_infected_bad.push_back(co_infection_bad);
+        
+        total_rates[9] += co_infection_bad;
+    } 
+
+    // done, now determine what to do by making a weighted distribution
+    // this will return a number between 0 and n_events - 1
+    // dependent on the relative weighting of each event
+    discrete_distribution<int> total_distribution(total_rates.begin(), total_rates.end());
+
+    // now sample a single sample from this discrete distribution
+    // this is the type of event that will be chosen
+    switch(total_distribution(rng_r))
+    {
+        // next, we now determine the actual individuals
+        // affected by the event
+        
+        case 0: // birth of susceptible
+            discrete_distribution <int> birth_susc_distribution(
+                    birth_rates_susceptible.begin()
+                    ,birth_rates_susceptible.end());
+
+            int S_idx = birth_susc_distribution(rng_r);
+
+            assert(S_idx >= 0);
+            assert(S_idx < Ns);
+
+            // execute the birth() function which also updates the stats
+            birth(Susceptible[S_idx], true);
+            break;
+
+        case 1: // infection of a susceptible by good plasmid
+            discrete_distribution <int> infection_susceptible_good_dist(
+                infection_rate_susceptible_good.begin()
+                infection_rate_susceptible_good.end());
+
+            int S_idx = infection_susceptible_good_dist(rng_r);
+
+            assert(S_idx >= 0);
+            assert(S_idx < Ns);
+
+            infection(Susceptible[S_idx], 
+
     }
-}
+       
+       
+        // end event_chooser(int const time_step)
 
 int main(int argc, char **argv)
 {
